7e267b074c2bce4d9dc34152cebaee1a54225a61
ietxe <ietxebarria@ikasle.ehu.eus>
2020-04-18 14:58:49 +0200

    Aldaketak cert

--- app.js
+++ app.js
@@ -38,10 +38,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(3030, function () {
-  console.log('Example app listening on port 3000!');
-});
-
 
 
 module.exports = app;
